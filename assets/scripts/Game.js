e64e32c6c5e0c315859e14c794e60b8a5d633a1b
coderyangl <798957387@qq.com>
2018-05-08 14:04:18 +0800

    基本版本完成

--- assets/scripts/Game.js
+++ assets/scripts/Game.js
@@ -29,23 +29,54 @@ cc.Class({
         player: {
             default: null,
             type: cc.Node
+        },
+        scoreDisplay: {
+            default: null,
+            type: cc.Label
+        },
+        // 得分音效资源
+        scoreAudio: {
+            default: null,
+            url: cc.AudioClip
         }
     },
 
     onLoad: function () {
         // 获取地平面的 y 轴坐标
-        this.groundY = this.ground.y + this.ground.height/2;
+        this.groundY = this.ground.y + this.ground.height / 2;
+
+        //初始化计时器
+        this.timer = 0;
+        this.startDuration = 0;
+
         // 生成一个新的星星
         this.spawnNewStar();
+        // 初始化计分
+        this.score = 0;
     },
 
-    spawnNewStar: function() {
+    gainScore: function () {
+        this.score += 1;
+        // 更新 scoreDisplay Label 的文字
+        cc.log(this.score.toString());
+        this.scoreDisplay.string = 'Score: ' + this.score.toString();
+        // 播放得分音效
+        cc.audioEngine.playEffect(this.scoreAudio, false);
+    },
+
+    spawnNewStar: function () {
         // 使用给定的模板在场景中生成一个新节点
         var newStar = cc.instantiate(this.starPrefab);
         // 将新增的节点添加到 Canvas 节点下面
         this.node.addChild(newStar);
         // 为星星设置一个随机位置
         newStar.setPosition(this.getNewStarPosition());
+        //将Game组件的示例传入星星组件
+        newStar.getComponent('Star').game = this;
+
+        //重置计时器,根据消失时间范围随机取一个值
+        this.startDuration = this.minStarDuration + cc.random0To1() * (this.maxStarDuration - this.minStarDuration);
+        this.timer = 0;
     },
 
     getNewStarPosition: function () {
@@ -53,9 +84,23 @@ cc.Class({
         // 根据地平面位置和主角跳跃高度，随机得到一个星星的 y 坐标
         var randY = this.groundY + cc.random0To1() * this.player.getComponent('Player').jumpHeight + 50;
         // 根据屏幕宽度，随机得到一个星星 x 坐标
-        var maxX = this.node.width/2;
+        var maxX = this.node.width / 2;
         randX = cc.randomMinus1To1() * maxX;
         // 返回星星坐标
         return cc.p(randX, randY);
+    },
+    update(dt) {
+        // 每帧更新计时器，超过限度还没有生成新的星星
+        // 就会调用游戏失败逻辑
+        if (this.timer > this.startDuration) {
+            this.gemeOver();
+
+            return;
+        }
+        this.timer += dt;
+    },
+    gemeOver :function () {
+        this.player.stopAllActions();
+        cc.director.loadScene('game');
     }
 });
--- assets/scripts/Star.js
+++ assets/scripts/Star.js
@@ -20,14 +20,40 @@ cc.Class({
             serializable: false
         }
     },
+    getPlayerDistance() {
+        //根据player节点位置判断距离
+        var playerPos = this.game.player.getPosition();
+        //根据两点位置计算两点距离
+        return cc.pDistance(this.node.position, playerPos);
+    },
+    onPicked() {
+
+        // 当星星被收集时，调用 Game 脚本中的接口，生成一个新的星星
+        this.game.spawnNewStar();
+        // 调用 Game 脚本的得分方法
+        this.game.gainScore();
+        this.node.destroy();
+    },
 
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {},
 
-    start () {
+    start() {
 
     },
 
-    // update (dt) {},
+    update(dt) {
+        //每帧判断和主角之间的距离
+        if (this.getPlayerDistance() < this.pickRadius) {
+            //调用收集逻辑
+            this.onPicked();
+            return;
+        }
+        // 根据 Game 脚本中的计时器更新星星的透明度
+        var opacityRatio = 1 - this.game.timer /this.game.startDuration;
+        var minOpacity = 50;
+        this.node.opacity = minOpacity + Math.floor(opacityRatio * (255 - minOpacity));
+
+    },
 });
